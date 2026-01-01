66bd30e279eae0ed420bfad13b54866c105e866d
Selith <selithrubasingha@gmail.com>
2026-01-01 19:53:08 +0530

    first attempt with drawing lines : barycentric coordinates

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include "tgaimage.h"
-
+#include <cmath>
+using namespace std;
 constexpr TGAColor white   = {255, 255, 255, 255}; // attention, BGRA order
 constexpr TGAColor green   = {  0, 255,   0, 255};
 constexpr TGAColor red     = {  0,   0, 255, 255};
 constexpr TGAColor blue    = {255, 128,  64, 255};
 constexpr TGAColor yellow  = {  0, 200, 255, 255};
+
+void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color){
+
+    for (float t=0;t<1;t+= 0.02){
+        int x = round( ax + (bx - ax)*t);
+        int y = round( ay + (by - ay)*t);
+        framebuffer.set(x,y,color);
+
+    }
+
+}
 
 int main(int argc, char** argv) {
     constexpr int width  = 64;
@@ -14,6 +26,11 @@ int main(int argc, char** argv) {
     int ax =  7, ay =  3;
     int bx = 12, by = 37;
     int cx = 62, cy = 53;
+
+    line(ax, ay, bx, by, framebuffer, blue);
+    line(cx, cy, bx, by, framebuffer, green);
+    line(cx, cy, ax, ay, framebuffer, yellow);
+    line(ax, ay, cx, cy, framebuffer, red);
 
     framebuffer.set(ax, ay, white);
     framebuffer.set(bx, by, white);
