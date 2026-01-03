4eb3855b2de95a1ffd27bc31b7f83ba0558160d0
Selith <selithrubasingha@gmail.com>
2026-01-03 14:10:15 +0530

    making squares into triangles with triangle-linear algebra

--- main.cpp
+++ main.cpp
@@ -52,6 +52,9 @@ void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color)
 
 }
 
+double signed_triangle_area(int ax, int ay, int bx, int by, int cx, int cy) {
+    return .5*((by-ay)*(bx+ax) + (cy-by)*(cx+bx) + (ay-cy)*(ax+cx));
+}
 
 
 void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuffer, TGAColor color) {
@@ -60,7 +63,9 @@ void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuf
     int bbminy = std::min(std::min(ay, by), cy); // defined by its top left and bottom right corners
     int bbmaxx = std::max(std::max(ax, bx), cx);
     int bbmaxy = std::max(std::max(ay, by), cy);
-/*
+    double total_area = signed_triangle_area(ax, ay, bx, by, cx, cy);
+
+    /*
 preproccer directives (just like include)
 tells the computer to do stuff before it starts compiling
 pragma tells the comp : "if you understand the instruction, do it . If not , ignore it "
@@ -70,6 +75,18 @@ omp - open multi processing (tells the comp. to use all the cores for the for lo
 #pragma omp parallel for 
         for (int x=bbminx; x<=bbmaxx; x++) {
             for (int y=bbminy; y<=bbmaxy; y++) {
+/*
+Barycentric coordinates 
+ are proportional to sub-triangle areas area(PCB), area(PCA)and area(PAB)
+. If all three coordinates are positive, 
+ lies within the triangle. If any barycentric coordinate is negative, 
+ lies outside the triangle.
+*/
+                double alpha = signed_triangle_area(x, y, bx, by, cx, cy) / total_area;
+                double beta  = signed_triangle_area(x, y, cx, cy, ax, ay) / total_area;
+                double gamma = signed_triangle_area(x, y, ax, ay, bx, by) / total_area;
+                if (alpha<0 || beta<0 || gamma<0) continue; // negative barycentric coordinate => the pixel is outside the triangle
+             
                 framebuffer.set(x, y, color);
             }
     }
