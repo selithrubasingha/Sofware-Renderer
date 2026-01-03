fff62d85ec33e7be10849363be841279014ee38d
Selith <selithrubasingha@gmail.com>
2026-01-03 13:02:31 +0530

    bottom half of triangle drawing complete

--- main.cpp
+++ main.cpp
@@ -61,9 +61,20 @@ void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuf
     if (ay>cy) { std::swap(ax, cx); std::swap(ay, cy); }
     if (by>cy) { std::swap(bx, cx); std::swap(by, cy); }
 
-    line(ax, ay, bx, by, framebuffer, color);
-    line(bx, by, cx, cy, framebuffer, color);
-    line(cx, cy, ax, ay, framebuffer, color);
+
+    //this is basically identical to my method 
+    // but this doesn't use the line method though
+    int total_height = cy - ay ;
+
+    if ( ay != by ){
+        int segment_height = by - ay ;
+        for (int y = ay ; y <= by ; y ++){
+            int x1 = ax +((cx - ax)*(y - ay)) / total_height ;
+            int x2 = ax + ((bx - ax)*(y - ay)) / segment_height;
+            for (int x=std::min(x1,x2); x<std::max(x1,x2); x++)  // draw a horizontal line
+                framebuffer.set(x, y, color);
+        }
+    }
 }
 
 int main(int argc, char** argv) {
