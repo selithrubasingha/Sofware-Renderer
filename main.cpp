c687ce4882ced1a33a6cff3d6c16c593a876e241
Selith <selithrubasingha@gmail.com>
2026-01-05 13:24:08 +0530

    added an grayscale tga file zbuffer

--- main.cpp
+++ main.cpp
@@ -13,59 +13,18 @@ IN THIS BRANCH ALL THE RASTERIZATION STUFF IN THE TUTORIAL IS COMPLETE
 constexpr int width  = 800;
 constexpr int height = 800;
 
-constexpr TGAColor white   = {255, 255, 255, 255};
-constexpr TGAColor red     = {  0,   0, 255, 255};
-constexpr TGAColor green   = {  0, 255,   0, 255};
-
-void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color){
-
-    bool steep = abs(ax-bx)<abs(ay-by);
-
-    if (steep){ // if the line is steep we tranpose the image??
-        swap(ax,ay);
-        swap(bx,by);
-    }
-
-    if (ax>bx){//making the ax smaller x and bx the alarger x all the time.
-        swap(ax,bx);
-        swap(ay,by);
-    }
-
-    
-    //The error variable is just tracking "How far away is the real line from the pixel I am currently drawing?"
-    int y = ay;
-    int ierror = 0;
-    for (int x=ax; x<=bx; x++) {
-        if (steep)
-            framebuffer.set(y,x,color);
-        else
-            framebuffer.set(x,y,color);
-        
-        /*
-        this ierror calculation is genius mathematics .
-        error is the (dy)/(dx)..if error is er > 0.5 ... then it's time to move to the next pixel 
-        but deviding in a computer is not that efficient ! 
-        so instead of dy/dx>0.5 ----> we do :: 2*dy >dx
-        */
-        ierror += 2 * abs(by-ay);
-        //if statement is removed and ternary is used ... because CPU is more efficient when ternary is used. 
-        y += (by > ay ? 1 : -1) * (ierror > bx - ax);
-        ierror -= 2 * (bx-ax)   * (ierror > bx - ax);
-
-}
-
-}
 
 double signed_triangle_area(int ax, int ay, int bx, int by, int cx, int cy) {
     return .5*((by-ay)*(bx+ax) + (cy-by)*(cx+bx) + (ay-cy)*(ax+cx));
 }
 
 // We accept vec4, but we only use v.x and v.y. 
-std::tuple<int,int> project(vec4 v) { 
+std::tuple<int,int,int> project(vec4 v) { 
     return { (v.x + 1.) * width/2, 
-             (v.y + 1.) * height/2 }; }
+             (v.y + 1.) * height/2,
+            (v.z + 1.) *   255./2 };  }
 
-void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuffer, TGAColor color) {
+void triangle(int ax, int ay, int az, int bx, int by, int bz, int cx, int cy, int cz, TGAImage &zbuffer, TGAImage &framebuffer, TGAColor color) {
 
     int bbminx = std::min(std::min(ax, bx), cx); // bounding box for the triangle
     int bbminy = std::min(std::min(ay, by), cy); // defined by its top left and bottom right corners
@@ -97,7 +56,8 @@ Barycentric coordinates
                 double beta  = signed_triangle_area(x, y, cx, cy, ax, ay) / total_area;
                 double gamma = signed_triangle_area(x, y, ax, ay, bx, by) / total_area;
                 if (alpha<0 || beta<0 || gamma<0) continue; // negative barycentric coordinate => the pixel is outside the triangle
-             
+                unsigned char z = static_cast<unsigned char>(alpha * az + beta * bz + gamma * cz);
+                zbuffer.set(x, y, {z});
                 framebuffer.set(x, y, color);
             }
     }
@@ -112,16 +72,18 @@ int main(int argc, char** argv) {
 
     Model model(argv[1]);
     TGAImage framebuffer(width, height, TGAImage::RGB);
+    TGAImage     zbuffer(width, height, TGAImage::GRAYSCALE);
 
     for (int i=0; i<model.nfaces(); i++) { // iterate through all triangles
-        auto [ax, ay] = project(model.vert(i, 0));
-        auto [bx, by] = project(model.vert(i, 1));
-        auto [cx, cy] = project(model.vert(i, 2));
+        auto [ax, ay, az] = project(model.vert(i, 0));
+        auto [bx, by, bz] = project(model.vert(i, 1));
+        auto [cx, cy, cz] = project(model.vert(i, 2));
         TGAColor rnd;
         for (int c=0; c<3; c++) rnd[c] = std::rand()%255;
-        triangle(ax, ay, bx, by, cx, cy, framebuffer, rnd);
+        triangle(ax, ay, az, bx, by, bz, cx, cy, cz, zbuffer, framebuffer, rnd);
     }
 
     framebuffer.write_tga_file("framebuffer.tga");
+    zbuffer.write_tga_file("zbuffer.tga");
     return 0;
 }
