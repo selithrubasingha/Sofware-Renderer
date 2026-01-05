fcc3a68229be2cdbe0d47a225c77e6a0eada32a6
Selith <selithrubasingha@gmail.com>
2026-01-05 11:32:44 +0530

    sorting the triangles in depth order for a more cleaner image

--- model.cpp
+++ model.cpp
@@ -1,6 +1,8 @@
+
 #include <fstream>
 #include <sstream>
 #include "model.h"
+#include <algorithm>
 
 Model::Model(const std::string filename) {
     std::ifstream in;
@@ -13,26 +15,14 @@ Model::Model(const std::string filename) {
         char trash;
         if (!line.compare(0, 2, "v ")) {
             iss >> trash;
-            vec4 v = {0,0,0,1};
+            vec4 v;
             for (int i : {0,1,2}) iss >> v[i];
             verts.push_back(v);
-        } else if (!line.compare(0, 3, "vn ")) {
-            iss >> trash >> trash;
-            vec4 n;
-            for (int i : {0,1,2}) iss >> n[i];
-            norms.push_back(normalized(n));
-        } else if (!line.compare(0, 3, "vt ")) {
-            iss >> trash >> trash;
-            vec2 uv;
-            for (int i : {0,1}) iss >> uv[i];
-            tex.push_back({uv.x, 1-uv.y});
         } else if (!line.compare(0, 2, "f ")) {
             int f,t,n, cnt = 0;
             iss >> trash;
             while (iss >> f >> trash >> t >> trash >> n) {
                 facet_vrt.push_back(--f);
-                facet_tex.push_back(--t);
-                facet_nrm.push_back(--n);
                 cnt++;
             }
             if (3!=cnt) {
@@ -42,15 +32,28 @@ Model::Model(const std::string filename) {
         }
     }
     std::cerr << "# v# " << nverts() << " f# "  << nfaces() << std::endl;
-    auto load_texture = [&filename](const std::string suffix, TGAImage &img) {
-        size_t dot = filename.find_last_of(".");
-        if (dot==std::string::npos) return;
-        std::string texfile = filename.substr(0,dot) + suffix;
-        std::cerr << "texture file " << texfile << " loading " << (img.read_tga_file(texfile.c_str()) ? "ok" : "failed") << std::endl;
-    };
-    load_texture("_diffuse.tga",    diffusemap );
-    load_texture("_nm_tangent.tga", normalmap);
-    load_texture("_spec.tga",       specularmap);
+
+    std::vector<int> idx(nfaces());    // permutation, a map from new to old facet indices
+    for (int i = 0 ; i<nfaces() ; i++) // we start with the identity
+        idx[i] = i;
+
+    std::sort(idx.begin(), idx.end(),
+              [&](const int& a, const int& b) { // given two triangles, compare their min z coordinate
+                  float aminz = std::min(vert(a, 0).z,
+                                         std::min(vert(a, 1).z,
+                                                  vert(a, 2).z));
+                  float bminz = std::min(vert(b, 0).z,
+                                         std::min(vert(b, 1).z,
+                                                  vert(b, 2).z));
+                  return aminz < bminz;
+              });
+
+    std::vector<int> facet_vrt2(nfaces()*3); // allocate an array to store permutated facets
+    for (int i=0; i<nfaces(); i++)           // for each (new) facet
+        for (int j=0; j<3; j++)              // copy its three vertices from the old array
+            facet_vrt2[i*3+j] = facet_vrt[idx[i]*3+j];
+
+    facet_vrt = facet_vrt2;                  // store the sorted triangles
 }
 
 int Model::nverts() const { return verts.size(); }
@@ -64,19 +67,87 @@ vec4 Model::vert(const int iface, const int nthvert) const {
     return verts[facet_vrt[iface*3+nthvert]];
 }
 
-vec4 Model::normal(const int iface, const int nthvert) const {
-    return norms[facet_nrm[iface*3+nthvert]];
-}
 
-vec4 Model::normal(const vec2 &uv) const {
-    TGAColor c = normalmap.get(uv[0]*normalmap.width(), uv[1]*normalmap.height());
-    return normalized(vec4{(double)c[2],(double)c[1],(double)c[0],0}*2./255. - vec4{1,1,1,0});
-}
 
-vec2 Model::uv(const int iface, const int nthvert) const {
-    return tex[facet_tex[iface*3+nthvert]];
-}
+// #include <fstream>
+// #include <sstream>
+// #include "model.h"
 
-const TGAImage& Model::diffuse()  const { return diffusemap;  }
-const TGAImage& Model::specular() const { return specularmap; }
+// Model::Model(const std::string filename) {
+//     std::ifstream in;
+//     in.open(filename, std::ifstream::in);
+//     if (in.fail()) return;
+//     std::string line;
+//     while (!in.eof()) {
+//         std::getline(in, line);
+//         std::istringstream iss(line.c_str());
+//         char trash;
+//         if (!line.compare(0, 2, "v ")) {
+//             iss >> trash;
+//             vec4 v = {0,0,0,1};
+//             for (int i : {0,1,2}) iss >> v[i];
+//             verts.push_back(v);
+//         } else if (!line.compare(0, 3, "vn ")) {
+//             iss >> trash >> trash;
+//             vec4 n;
+//             for (int i : {0,1,2}) iss >> n[i];
+//             norms.push_back(normalized(n));
+//         } else if (!line.compare(0, 3, "vt ")) {
+//             iss >> trash >> trash;
+//             vec2 uv;
+//             for (int i : {0,1}) iss >> uv[i];
+//             tex.push_back({uv.x, 1-uv.y});
+//         } else if (!line.compare(0, 2, "f ")) {
+//             int f,t,n, cnt = 0;
+//             iss >> trash;
+//             while (iss >> f >> trash >> t >> trash >> n) {
+//                 facet_vrt.push_back(--f);
+//                 facet_tex.push_back(--t);
+//                 facet_nrm.push_back(--n);
+//                 cnt++;
+//             }
+//             if (3!=cnt) {
+//                 std::cerr << "Error: the obj file is supposed to be triangulated" << std::endl;
+//                 return;
+//             }
+//         }
+//     }
+//     std::cerr << "# v# " << nverts() << " f# "  << nfaces() << std::endl;
+//     auto load_texture = [&filename](const std::string suffix, TGAImage &img) {
+//         size_t dot = filename.find_last_of(".");
+//         if (dot==std::string::npos) return;
+//         std::string texfile = filename.substr(0,dot) + suffix;
+//         std::cerr << "texture file " << texfile << " loading " << (img.read_tga_file(texfile.c_str()) ? "ok" : "failed") << std::endl;
+//     };
+//     load_texture("_diffuse.tga",    diffusemap );
+//     load_texture("_nm_tangent.tga", normalmap);
+//     load_texture("_spec.tga",       specularmap);
+// }
+
+// int Model::nverts() const { return verts.size(); }
+// int Model::nfaces() const { return facet_vrt.size()/3; }
+
+// vec4 Model::vert(const int i) const {
+//     return verts[i];
+// }
+
+// vec4 Model::vert(const int iface, const int nthvert) const {
+//     return verts[facet_vrt[iface*3+nthvert]];
+// }
+
+// vec4 Model::normal(const int iface, const int nthvert) const {
+//     return norms[facet_nrm[iface*3+nthvert]];
+// }
+
+// vec4 Model::normal(const vec2 &uv) const {
+//     TGAColor c = normalmap.get(uv[0]*normalmap.width(), uv[1]*normalmap.height());
+//     return normalized(vec4{(double)c[2],(double)c[1],(double)c[0],0}*2./255. - vec4{1,1,1,0});
+// }
+
+// vec2 Model::uv(const int iface, const int nthvert) const {
+//     return tex[facet_tex[iface*3+nthvert]];
+// }
+
+// const TGAImage& Model::diffuse()  const { return diffusemap;  }
+// const TGAImage& Model::specular() const { return specularmap; }
 
